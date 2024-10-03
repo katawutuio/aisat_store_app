d35891fdfa0e49e59a50d53773d65178245e1587
Katawut Keng <katawutu.io@gmail.com>
2024-10-04 01:42:00 +0700

    ทำให้ icont ที่ bottomNavigationBar สามารถคลิกไ้ด้ - คลิกแล้วให้เปลี่ยนค่า _pageValue ด้วยการ setState - ปรับสีของ icon เมื่อกด

--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -8,42 +8,53 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _pageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar:
-          BottomNavigationBar(type: BottomNavigationBarType.fixed, items: [
-        BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icons/home.png',
-              width: 25,
-            ),
-            label: "Home"),
-        BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icons/love.png',
-              width: 25,
-            ),
-            label: "Favorite"),
-        BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icons/mart.png',
-              width: 25,
-            ),
-            label: "Stores"),
-        BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icons/cart.png',
-              width: 25,
-            ),
-            label: "Cart"),
-        BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/icons/user.png',
-              width: 25,
-            ),
-            label: "Account"),
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.purple,
+          unselectedItemColor: Colors.grey,
+          currentIndex: _pageIndex,
+          onTap: (value) {
+            setState(() {
+              _pageIndex = value;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icons/home.png',
+                  width: 25,
+                ),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icons/love.png',
+                  width: 25,
+                ),
+                label: "Favorite"),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icons/mart.png',
+                  width: 25,
+                ),
+                label: "Stores"),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icons/cart.png',
+                  width: 25,
+                ),
+                label: "Cart"),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icons/user.png',
+                  width: 25,
+                ),
+                label: "Account"),
+          ]),
     );
   }
 }
