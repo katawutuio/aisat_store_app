07a4072db58f8ec2c1cc9d76b7293d06aa495379
Katawut Keng <katawutu.io@gmail.com>
2024-10-05 16:01:24 +0700

    - ใช้ Positioned เพื่อระตุตำแหน่งของ widget  บน stack ได้ -  TextField vs TextFormField ต่างกันที่ TextFormField สามารทำ validation ได้ ถ้าไม่มี validate ก๋ใช้ TextField ได้ - ใช้ SizedBox ครอบ TextField เพื่อจัดชนาด - ใช้ InputDecoration เพื่อแต่ง TextField - ใน InputDecoration เพิ่ม property ต่าง ๆ เพื่อใช้จัดการ style ใน TextField

--- lib/views/screens/nav_screen/widgets/header_widget.dart
+++ lib/views/screens/nav_screen/widgets/header_widget.dart
@@ -15,6 +15,36 @@ class HeaderWidget extends StatelessWidget {
             width: MediaQuery.of(context).size.width,
             fit: BoxFit.cover,
           ),
+          Positioned(
+            left: 48,
+            top: 68,
+            child: SizedBox(
+              width: 250,
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter text',
+                  hintStyle: const TextStyle(
+                    fontSize: 14,
+                    color: Color(0xFF7F7F7F),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 16,
+                  ),
+                  prefixIcon: Image.asset(
+                    'assets/icons/search1.png',
+                  ),
+                  suffixIcon: Image.asset(
+                    'assets/icons/cam.png',
+                  ),
+                  fillColor: Colors.grey.shade200,
+                  filled: true,
+                  focusColor: Colors.black,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
